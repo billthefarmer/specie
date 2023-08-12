85a3a23a9b7353c04f1dbd961658440ccf90939b
Bill Farmer <williamjfarmer@yahoo.co.uk>
2023-08-12 11:06:28 +0100

    Update version, changelog, README

--- src/main/java/org/billthefarmer/specie/SpecieWidgetRefresh.java
+++ src/main/java/org/billthefarmer/specie/SpecieWidgetRefresh.java
@@ -28,7 +28,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
